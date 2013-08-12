2eefdf8b20def18de8fd162af72d0a4f8ae45557
Maciej Bliziński <maciej@opencsw.org>
2013-08-13 00:07:19 +0100

    Fix csw-upload-pkg unit tests.

--- lib/python/csw_upload_pkg.py
+++ lib/python/csw_upload_pkg.py
@@ -136,7 +136,7 @@ class Srv4Uploader(object):
     for filename in self.filenames:
       self._ImportMetadata(filename)
       md5_sum = self._GetFileMd5sum(filename)
-      self._rest_client.RegisterInRelationalDb(md5_sum)
+      self._rest_client.RegisterLevelTwo(md5_sum)
       file_in_allpkgs, file_metadata = self._GetSrv4FileMetadata(md5_sum)
       if file_in_allpkgs:
         logging.debug("File %s already uploaded.", filename)
